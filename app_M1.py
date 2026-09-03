1d2b3e727615cce8d41002d12777279f00418fd9
Eder.Torres <edertorresguerrero@gmail.com>
2026-09-03 01:18:49 -0500

    Update app_M1.py

--- app_M1.py
+++ app_M1.py
@@ -1,1 +1,17 @@
 import streamlit as st
+
+st.title("Bootcamp Data Analytics for Oil & Gas")
+
+pozo= "SPE-001"
+petroleo_bpd = 1250
+agua_bpd = 350.50
+status = True
+liquido_total_bpd = petroleo_bpd + agua_bpd
+corte_agua_pct = (agua_bpd / liquido_total_bpd) * 100
+
+st.write(pozo)
+st.write(petroleo_bpd)
+st.write(agua_bpd)
+st.write(status)
+st.write(liquido_total_bpd)
+st.write(corte_agua_pct)
