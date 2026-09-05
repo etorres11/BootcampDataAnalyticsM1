8eda150ef85258ee4fb6898a921ac1260037e1fa
Eder.Torres <edertorresguerrero@gmail.com>
2026-09-04 22:13:39 -0500

    Update app_M1.py

--- app_M1.py
+++ app_M1.py
@@ -7,18 +7,18 @@ modulos = st.sidebar.selectbox("seleccione un módulo", ["Introducción de varia
 
 if modulos == "Introducción de variables":
 
-pozo= "SPE-001"
-petroleo_bpd = 1250
-agua_bpd = 350.50
-status = True
-liquido_total_bpd = petroleo_bpd + agua_bpd
-corte_agua_pct = (agua_bpd / liquido_total_bpd) * 100
+  pozo = "SPE-001"
+  petroleo_bpd = 1250
+  agua_bpd = 350.50
+  status = True
+  liquido_total_bpd = petroleo_bpd + agua_bpd
+  corte_agua_pct = (agua_bpd / liquido_total_bpd) * 100
 
-st.write(pozo)
-st.write(petroleo_bpd)
-st.write(agua_bpd)
-st.write(status)
-st.write(liquido_total_bpd)
-st.write(corte_agua_pct)
+  st.write(pozo)
+  st.write(petroleo_bpd)
+  st.write(agua_bpd)
+  st.write(status)
+  st.write(liquido_total_bpd)
+  st.write(corte_agua_pct)
 
 elif modulos == "Funciones":
