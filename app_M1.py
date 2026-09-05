24882fd3afde2f9d6cd66a471b47689f632e689a
Eder.Torres <edertorresguerrero@gmail.com>
2026-09-04 22:32:47 -0500

    Update app_M1.py

--- app_M1.py
+++ app_M1.py
@@ -50,7 +50,7 @@ elif modulos == "Funciones":
   presion_fondo = st.number_input("Ingrese la presión de fondo fluyente", min_value = 0, max_value = 9000, value = 1500)
   decimales = st.slider("Selecciones la cantidad de decimales para su resultado", min_value = 0, max_value = 4, value = 2)
 
+caudal = calcular_caudal_vogel(caudal_maximo, presion_yacimiento, presion_fondo, decimales)
 
-
-
+st.write("El caudal es: ", caudal)
 
