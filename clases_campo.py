bd52731438b9e48b553ca66afcbc44df42680051
Eder.Torres <edertorresguerrero@gmail.com>
2026-09-07 16:24:29 -0500

    Update clases_campo.py

--- clases_campo.py
+++ clases_campo.py
@@ -4,7 +4,7 @@ import pandas as pd
 from funciones_calculos import (calcular_liquido, calcular_bsw, calcular_gor, proyectar_produccion)
 from funciones_datos import (filtrar_pozo, resumen_dataframe, exportar_excel)
 from clases_pozo import Pozo
-from clases_campo import Campo
+
 
 st.title("Bootcamp Data Analytics for Oil & Gas")
 st.sidebar.title("Parámetros")
